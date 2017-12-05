8aa61dc835bcdf305b6eddfbb5dd95625cc7d739
Pablo Varela <pablovarela182@gmail.com>
2017-12-05 19:02:26 +0100

    Django structure

---
+++ src/movies/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True) # optional
+
+
+class Movie(models.Model):
+
+    name = models.CharField(max_length=150)
+    summary = models.TextField()
+    director_name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    image = models.URLField()
+    rating = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
